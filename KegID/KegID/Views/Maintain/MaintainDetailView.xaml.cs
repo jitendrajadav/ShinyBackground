2a63b14c4464eb8729b2b9c7f0e3325cb3366894
Jitendra Jadav <jitendra.jadav@outlook.com>
2018-03-15 20:01:59 +0530

    Many UI enhancement for android app

--- KegID/KegID/Views/Maintain/MaintainDetailView.xaml.cs
+++ KegID/KegID/Views/Maintain/MaintainDetailView.xaml.cs
@@ -7,30 +7,28 @@ using Xamarin.Forms.Xaml;
 namespace KegID.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MaintainDetailView : ContentPage
-	{
-		public MaintainDetailView()
-		{
-			InitializeComponent ();
+    public partial class MaintainDetailView : ContentPage
+    {
+        public MaintainDetailView()
+        {
+            InitializeComponent();
             GenerateDynamicMaintenancePerformed();
-
         }
 
         public void GenerateDynamicMaintenancePerformed()
         {
-            //maintenancePerformed;
-
             foreach (var item in SimpleIoc.Default.GetInstance<MaintainViewModel>().MaintenancePerformedCollection)
             {
                 Label PerformedLabel = new Label()
                 {
                     VerticalOptions = LayoutOptions.Center,
-                    Text = item
+                    Text = item,
+                    Style = (Style)Application.Current.Resources["LabelTitleStyle"],
+                    TextColor = Color.Black
                 };
 
                 maintenancePerformedStack.Children.Add(PerformedLabel);
             }
-
         }
     }
 }
