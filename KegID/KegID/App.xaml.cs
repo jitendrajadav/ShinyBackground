9411f718d6c1488a79ce991505d61a6105fb6de8
Jitendra Jadav <jitendra.jadav@outlook.com>
2018-04-18 19:19:57 +0530

    Working on Map

--- KegID/KegID/App.xaml.cs
+++ KegID/KegID/App.xaml.cs
@@ -72,7 +72,7 @@ namespace KegID
             // Handle when your app starts
             SQLiteServiceClient.Instance.CreateDbIfNotExist();
             Geolocation.GetGPS();
-           var value= Geolocation.GetCurrentLocation();
+           //var value= Geolocation.GetCurrentLocation();
         }
 
         protected override void OnSleep ()
