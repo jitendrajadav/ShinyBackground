3cdd758190440a3b08c9ea5de9d1d4571890c5f6
Jitendra Jadav <jitendra.jadav@outlook.com>
2018-10-08 17:01:56 +0530

    Changed project type for UI test

--- KegID/KegID.UITest/Tests.cs
+++ KegID/KegID.UITest/Tests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
@@ -13,6 +12,10 @@ namespace KegID.UITest
     {
         IApp app;
         Platform platform;
+
+        static readonly Func<AppQuery, AppQuery> InitialMessage = c => c.Marked("MyLabel").Text("Hello, Xamarin.Forms!");
+        static readonly Func<AppQuery, AppQuery> Button = c => c.Marked("loginButton");
+        static readonly Func<AppQuery, AppQuery> DoneMessage = c => c.Marked("loginButton").Text("Login");
 
         public Tests(Platform platform)
         {
@@ -33,5 +36,22 @@ namespace KegID.UITest
 
             Assert.IsTrue(results.Any());
         }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Repl();
+            // Arrange - Nothing to do because the queries have already been initialized.
+            AppResult[] result = app.Query(InitialMessage);
+            Assert.IsTrue(result.Any(), "The initial message string isn't correct - maybe the app wasn't re-started?");
+
+            // Act
+            app.Tap(Button);
+
+            // Assert
+            result = app.Query(DoneMessage);
+            Assert.IsTrue(result.Any(), "The 'clicked' message is not being displayed.");
+        }
     }
+
 }
