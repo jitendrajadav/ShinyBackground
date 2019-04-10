f9084ab7259c2441b67dc84ad7307414a26d3d83
Jitendra Jadav <jitendra.jadav@slg.com>
2019-04-10 14:34:12 +0530

    Added custome code for nullable

---
+++ KegID/KegID.iOS/Extensions/CheckColorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UIKit;
+using Xamarin.Forms;
+
+namespace KegID.iOS.Extensions
+{
+    /// <summary>
+    /// Color extensions
+    /// </summary>
+    public static class CheckColorExtensions
+    {
+        internal static readonly UIColor SeventyPercentGrey = new UIColor(0.7f, 0.7f, 0.7f, 1);
+        public static bool IsDefault(this Color color) => Color.Default == color;
+    }
+}
