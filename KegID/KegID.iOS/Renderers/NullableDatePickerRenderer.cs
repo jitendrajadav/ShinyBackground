5f498d87d8f4af3d4f4fffab569766f907f1c606
Jitendra Jadav <jitendra.jadav@slg.com>
2019-04-22 20:42:35 +0530

    Fixed the issue with null date

---
+++ KegID/KegID.iOS/Renderers/NullableDatePickerRenderer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using KegID.Common;
+using KegID.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NullableDatePicker), typeof(NullableDatePickerRenderer))]
+namespace KegID.iOS.Renderers
+{
+    public class NullableDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && this.Control != null)
+            {
+                this.AddClearButton();
+
+                this.Control.BorderStyle = UITextBorderStyle.Line;
+                Control.Layer.BorderColor = UIColor.LightGray.CGColor;
+                Control.Layer.BorderWidth = 1;
+
+                var entry = (NullableDatePicker)this.Element;
+                if (!entry.NullableDate.HasValue)
+                {
+                    this.Control.Text = entry.PlaceHolder;
+                }
+
+                if (Device.Idiom == TargetIdiom.Tablet)
+                {
+                    this.Control.Font = UIFont.SystemFontOfSize(25);
+                }
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // Check if the property we are updating is the format property
+            if (e.PropertyName == Xamarin.Forms.DatePicker.DateProperty.PropertyName || e.PropertyName == Xamarin.Forms.DatePicker.FormatProperty.PropertyName)
+            {
+                var entry = (NullableDatePicker)this.Element;
+
+                // If we are updating the format to the placeholder then just update the text and return
+                if (this.Element.Format == entry.PlaceHolder)
+                {
+                    this.Control.Text = entry.PlaceHolder;
+                    return;
+                }
+            }
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        private void AddClearButton()
+        {
+            var originalToolbar = this.Control.InputAccessoryView as UIToolbar;
+
+            if (originalToolbar != null && originalToolbar.Items.Length <= 2)
+            {
+                var clearButton = new UIBarButtonItem("Clear", UIBarButtonItemStyle.Plain, ((sender, ev) =>
+                {
+                    NullableDatePicker baseDatePicker = this.Element as NullableDatePicker;
+                    this.Element.Unfocus();
+                    this.Element.Date = DateTime.Now;
+                    baseDatePicker.CleanDate();
+
+                }));
+
+                var newItems = new List<UIBarButtonItem>();
+                foreach (var item in originalToolbar.Items)
+                {
+                    newItems.Add(item);
+                }
+
+                newItems.Insert(0, clearButton);
+
+                originalToolbar.Items = newItems.ToArray();
+                originalToolbar.SetNeedsDisplay();
+            }
+
+        }
+    }
+
+}
