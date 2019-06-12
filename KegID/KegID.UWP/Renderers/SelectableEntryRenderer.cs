4d043b892883e7d1ad692e6b8cc9cc36bc1c1edf
Jitendra Jadav <jitendra.jadav@slg.com>
2019-06-12 16:56:37 +0530

    Make it editable

---
+++ KegID/KegID.UWP/Renderers/SelectableEntryRenderer.cs
@@ -0,0 +1,20 @@
+﻿using KegID.Common;
+using KegID.UWP.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(SelectableEntry), typeof(SelectableEntryRenderer))]
+namespace KegID.UWP.Renderers
+{
+    public class SelectableEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement.IsFocused
+               && Control != null && Control.FocusState != Windows.UI.Xaml.FocusState.Unfocused)
+                Control.SelectAll();
+        }
+
+    }
+}
